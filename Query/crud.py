a3e9532758862ad64eda7f05355f0fb39e2f0be8
Nicolò Salvi <nicolo.salvi@edu.unife.it>
2023-05-23 16:19:25 +0200

    Migliorato menu

--- Query/crud.py
+++ Query/crud.py
@@ -1,47 +1,52 @@
 import pyodbc
+from time import sleep
 server = 'localhost'
 database = 'HR'
 username = 'tbd'
 password = 'TechBD'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
-print ('Seleziona operazione da eseguire:')
-print ('[1] Insert')
-print ('[2] Update')
-print ('[3] Select')
-print ('[4] Delete')
-operation = int(input())
 
-if operation == 1:
-    print ('Inserting a new row into table jobs')
-    #Insert Query
-    tsql = "INSERT INTO jobs (job_title, min_salary, max_salary) VALUES (?,?,?);"
-    with cursor.execute(tsql,'Spennapolli','1000','10000'):
-        print ('Successfully Inserted!')
-
-elif operation == 2:
-    #Update Query
-    print ('Updating phone number for William')
-    tsql = "UPDATE employees SET phone_number = ? WHERE first_name = ?"
-    with cursor.execute(tsql,'111222333444','William'):
-        print ('Successfully Updated!')
-
-elif operation == 3:
-    #Select Query
-    print ('Reading data from table locations')
-    tsql = "SELECT * FROM locations;"
-    with cursor.execute(tsql):
-        row = cursor.fetchone()
-        while row:
-            print (str(row[0]) + " " + str(row[1]))
+ans=True
+while ans:
+    print ('Seleziona operazione da eseguire:')
+    print ('[1] Insert')
+    print ('[2] Update')
+    print ('[3] Select')
+    print ('[4] Delete')
+    print ('[0] Esci')
+    ans=input() 
+    if ans=="1": 
+        print ('Inserting a new row into table jobs')
+        #Insert Query
+        tsql = "INSERT INTO jobs (job_title, min_salary, max_salary) VALUES (?,?,?);"
+        with cursor.execute(tsql,'Spennapolli','1000','10000'):
+            print ('Successfully Inserted!\n')
+    elif ans=="2":
+        #Update Query2
+        print ('Updating phone number for William')
+        tsql = "UPDATE employees SET phone_number = ? WHERE first_name = ?"
+        with cursor.execute(tsql,'111222333444','William'):
+            print ('Successfully Updated!\n')
+            sleep(1)
+    elif ans=="3":
+        #Select Query
+        print ('Reading data from table locations')
+        tsql = "SELECT * FROM locations;"
+        with cursor.execute(tsql):
             row = cursor.fetchone()
-
-elif operation == 4:
-    #Delete Query
-    print ('Deleting Spennapolli')
-    tsql = "DELETE FROM jobs WHERE job_title = ?"
-    with cursor.execute(tsql,'Spennapolli'):
-        print ('Successfully Deleted!')
-
-else:
-    print('Adios')
+            while row:
+                print (str(row[0]) + " " + str(row[1]))
+                row = cursor.fetchone()
+    elif ans=="4":
+        #Delete Query
+        print ('Deleting Spennapolli')
+        tsql = "DELETE FROM jobs WHERE job_title = ?"
+        with cursor.execute(tsql,'Spennapolli'):
+            print ('Successfully Deleted!')
+    elif ans =="0":
+      print("Adios") 
+      ans = None
+    else:
+        print("Not Valid Choice Try again")
+        sleep(1)
