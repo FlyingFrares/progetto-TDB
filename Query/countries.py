3bdd4f371e6edb61b722a18422ef022494139753
Nicolò Salvi <nicolo.salvi@edu.unife.it>
2023-05-24 03:03:32 +0200

    Implementazione countries

---
+++ Query/countries.py
@@ -0,0 +1,77 @@
+import pyodbc
+from time import sleep
+
+server = 'localhost'
+database = 'HR'
+username = 'tbd'
+password = 'TechBD'
+try:
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+except pyodbc.Error as ex:
+    sqlstate = ex.args[0]
+    if sqlstate == '28000':
+        print("Connessione per " + username + " fallita")
+else:
+    print('Tabella countries')
+    ans=True
+    while ans:
+        print ('Seleziona operazione da eseguire:')
+        print ('[1] Select')
+        print ('[2] Insert')
+        print ('[3] Delete')
+        print ('[0] Esci')
+        ans=input()
+
+        if ans=="1": 
+            #Select Query
+            country = input('Inserisci nazione da cercare:')
+            try:
+                tsql = "SELECT * FROM countries WHERE country_name LIKE ?;"
+                cursor.execute(tsql,'%'+country+'%')
+                row = cursor.fetchone()
+                while row:
+                    print (str(row[0]) + "  " + str(row[1]) + "  " + str(row[2]))
+                    row = cursor.fetchone()
+            except pyodbc.Error as ex:
+                print(ex.args[1])
+            print()
+            sleep(1)
+            
+        elif ans=="2":
+            #Insert Query
+            print ('Inserisci un nuovo record')
+            country_id = input('country_id [char(2)]: ')
+            country_name = input('country_name: ')
+            region_id = int(input('region_id [int 1..4]: '))
+            try:
+                tsql = "INSERT INTO countries VALUES (?,?,?);"
+                cursor.execute(tsql,country_id,country_name,region_id)
+                print ('Record inserito correttamente\n')
+            except pyodbc.Error as ex:
+                print(ex.args[1])
+            sleep(1)
+           
+        elif ans=="3":
+            #Delete Query
+            country_id = input('Inserisci il country_id [char(2)] da eliminare: ')
+            try:
+                tsql = "DELETE FROM countries WHERE country_id = ?;"
+                cursor.execute(tsql,country_id)
+                cursor.execute("select @@rowcount")
+                rowcount = cursor.fetchall()[0][0]
+                if rowcount == 0: 
+                    print('Nessun record eliminato\n')
+                else:
+                    print ('Record eliminato\n')
+            except pyodbc.Error as ex:
+                print(ex.args[1])
+            sleep(1)
+
+        elif ans =="0":
+            print("Adios") 
+            ans = None
+
+        else:
+            print("Selezione non valida\n")
+            sleep(1)
