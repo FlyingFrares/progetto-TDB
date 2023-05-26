d12f3fce76700295849a9411eeae296b594a3cfe
Nicolò Salvi <nicolo.salvi@edu.unife.it>
2023-05-26 19:35:07 +0200

    Aggiunta operazione di JOIN e correzioni

--- Query/countries.py
+++ Query/countries.py
@@ -13,7 +13,7 @@ except pyodbc.Error as ex:
     if sqlstate == '28000':
         print("Connessione per " + username + " fallita")
 else:
-    print('Tabella countries')
+    print('\nTabella countries')
     ans=True
     while ans:
         print ('Seleziona operazione da eseguire:')
@@ -25,19 +25,21 @@ else:
 
         if ans=="1": 
             #Select Query
-            country = input('Inserisci nazione da cercare:')
+            country = input('Inserisci nazione da cercare: ')
+            tsql =  "SELECT c.country_id, c.country_name, r.region_name " +\
+                    "FROM countries AS c, regions AS r " +\
+                    "WHERE c.region_id = r.region_id AND c.country_name LIKE ?;"
             try:
-                tsql = "SELECT * FROM countries WHERE country_name LIKE ?;"
-                cursor.execute(tsql,'%'+country+'%')
-                row = cursor.fetchone()
-                while row:
-                    print (str(row[0]) + "  " + str(row[1]) + "  " + str(row[2]))
+                with cursor.execute(tsql,'%'+country+'%'):
+                    print('\ncountry_id | country_name | region_id')
                     row = cursor.fetchone()
-                    sleep(0.2)
+                    while row:
+                        print (str(row[0]) + " | " + str(row[1]) + " | " + str(row[2]))
+                        row = cursor.fetchone()
+                        sleep(0.2)
             except pyodbc.Error as ex:
                 print(ex.args[1])
-            print()
-            sleep(1)
+            print('-----------------------------------------')       
             
         elif ans=="2":
             #Insert Query
@@ -45,35 +47,35 @@ else:
             country_id = input('country_id [char(2)]: ')
             country_name = input('country_name: ')
             region_id = int(input('region_id [int 1..4]: '))
+            tsql = "INSERT INTO countries VALUES (?,?,?);"
             try:
-                tsql = "INSERT INTO countries VALUES (?,?,?);"
-                cursor.execute(tsql,country_id.upper(),country_name,region_id)
-                print ('Record inserito correttamente\n')
+                with cursor.execute(tsql,country_id.upper(),country_name,region_id):
+                    print ('Record inserito correttamente\n')
             except pyodbc.Error as ex:
                 print(ex.args[1])
-            sleep(0.5)
            
         elif ans=="3":
             #Delete Query
             country_id = input('Inserisci il country_id [char(2)] da eliminare: ')
-            try:
-                tsql = "DELETE FROM countries WHERE country_id = ?;"
-                cursor.execute(tsql,country_id)
-                cursor.execute("select @@rowcount")
-                rowcount = cursor.fetchall()[0][0]
-                if rowcount == 0: 
-                    print('Nessun record presente con country id '+ country_id +'\n')
-                else:
-                    print ('Record eliminato\n')
+            tsql = "DELETE FROM countries WHERE country_id = ?;"
+            try: 
+                with cursor.execute(tsql,country_id.upper()):
+                    with cursor.execute("select @@rowcount"):
+                        rowcount = cursor.fetchall()[0][0]
+                        if rowcount == 0: 
+                            print('Nessun record presente con country_id '+ country_id.upper() +'\n')
+                        else:
+                            print ('Record eliminato correttamente\n')
             except pyodbc.Error as ex:
                 print(ex.args[1])
-            sleep(0.5)
 
         elif ans =="0":
-            print("Adios\n")
-            sleep(0.5) 
+            #Exit
+            print('Adios\n')
             ans = None
 
         else:
-            print("Selezione non valida\n")
-            sleep(0.5)
+            #Retry
+            print('Selezione non valida, riprova\n')
+        
+        sleep(0.5)
