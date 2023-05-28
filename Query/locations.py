f03bd357b77de6a3ad0ba2d40f92d696a601be71
Nicolò Salvi <nicolo.salvi@edu.unife.it>
2023-05-28 18:25:27 +0200

    Creato locations.py

---
+++ Query/locations.py
@@ -0,0 +1,118 @@
+import pyodbc
+from time import sleep
+
+server = 'localhost'
+database = 'HR'
+username = 'tbd'
+password = 'TechBD'
+try:
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+except pyodbc.Error as ex:
+    sqlstate = ex.args[0]
+    if sqlstate == '28000':
+        print("Connessione per " + username + " fallita")
+else:
+    print('\nTabella locations')
+    ans=True
+    while ans:
+        print ('Seleziona operazione da eseguire:')
+        print ('[1] Select')
+        print ('[2] Insert')
+        print ('[3] Delete')
+        print ('[4] Update')
+        print ('[0] Esci')
+        ans=input('Input: ')
+
+        if ans=="1": 
+            #Select Query
+            location = input('Inserisci la nazione da cercare: ')
+            tsql =  "SELECT l.location_id, l.street_address, l.postal_code, l.city, l.state_province, c.country_name, r.region_name " +\
+                    "FROM locations AS l JOIN countries AS c ON l.country_id = c.country_id " +\
+                    "JOIN regions AS r ON c.region_id = r.region_id " +\
+                    "WHERE c.country_name LIKE ?;"
+            try:
+                with cursor.execute(tsql,'%'+location+'%'):
+                    print('\nlocation_id | street_address, postal_code, city, state_province, country_name, region_name')
+                    row = cursor.fetchone()
+                    while row:
+                        print (str(row[0]) + ", " + str(row[1]) + ", " + str(row[2]) + ", " +
+                               str(row[3]) + ", " + str(row[4]) + ", " + str(row[5]) + ", " + str(row[6]))
+                        row = cursor.fetchone()
+                        sleep(0.2)
+            except pyodbc.Error as ex:
+                print(ex.args[1])
+            print('---------------------------------------------------------------------------------------------------')       
+            
+        elif ans=="2":
+            #Insert Query
+            print ('Inserisci un nuovo record')
+            street_address = input('Inserisci lo street_address [varchar(40)]: ')
+            postal_code = input('Inserisci il postal_code [varchar(12)]: ')
+            city = input('Inserisci la città [varchar(30)]: ')
+            state_province = input('Inserisci lo state_province [varchar(25)]: ')
+            country_id = input('Inserisci il country_id [varchar(2)]: ')
+            tsql = "INSERT INTO locations VALUES (?,?,?,?,?);"
+            try:
+                with cursor.execute(tsql,street_address,postal_code,city,state_province,country_id.upper()):
+                    print ('Record inserito correttamente\n')
+            except pyodbc.Error as ex:
+                print(ex.args[1])
+           
+        elif ans=="3":
+            #Delete Query
+            location_id = int(input('Insserisci la location_id del record da eliminare: '))
+            tsql = "DELETE FROM locations WHERE location_id = ?;" 
+            try: 
+                with cursor.execute(tsql,location_id):
+                    with cursor.execute("select @@rowcount"):
+                        rowcount = cursor.fetchall()[0][0]
+                        if rowcount == 0: 
+                            print('Nessun record presente con la location_id inserita\n')
+                        else:
+                            print ('Record eliminato correttamente\n')
+            except pyodbc.Error as ex:
+                print(ex.args[1])
+                    
+        elif ans=="4":
+            #Update Query
+            location_id = int(input('Inserisci la location_id del record da aggiornare: '))
+            tsql = "SELECT * FROM locations WHERE location_id = ?;"
+            try: 
+                with cursor.execute(tsql,location_id):
+                    row = cursor.fetchone()
+                    with cursor.execute("select @@rowcount"):
+                        rowcount = cursor.fetchall()[0][0]
+                        if rowcount == 0: 
+                            print('Nessun record presente con la location_id inserita\n')
+                        else:
+                            print('\nstreet_address, postal_code, city, state_province, country_id')
+                            print ( str(row[1]) + ", " + str(row[2]) + ", " + str(row[3]) + ", " +
+                                    str(row[4]) + ", " + str(row[5]))
+                            sleep(0.2)
+                            print('\nInserisci i nuovi valori')
+                            street_address = input('Inserisci lo street_address [varchar(40)]: ')
+                            postal_code = input('Inserisci il postal_code [varchar(12)]: ')
+                            city = input('Inserisci la città [varchar(30)]: ')
+                            state_province = input('Inserisci lo state_province [varchar(25)]: ')
+                            country_id = input('Inserisci il country_id [varchar(2)]: ')
+                            tsql =  "UPDATE locations SET street_address = ?, postal_code = ?, city = ?, " +\
+                                    "state_province = ?, country_id = ? WHERE location_id = ?;"
+                            try:
+                                with cursor.execute(tsql,street_address,postal_code,city,state_province,country_id.upper(),location_id):
+                                    print ('Record aggiornato correttamente\n')
+                            except pyodbc.Error as ex:
+                                print(ex.args[1])
+            except pyodbc.Error as ex:
+                print(ex.args[1])
+
+        elif ans =="0":
+            #Exit
+            print('Adios\n')
+            ans = None
+
+        else:
+            #Retry
+            print('Selezione non valida, riprova\n')
+        
+        sleep(0.5)
--- Query/menu.py
+++ Query/menu.py
@@ -1,13 +1,35 @@
 import os
 
-print("Database HR")
-print("Seleziona la tabella su cui operare:")
-print ('[1] countires')
-print ('[2] departments')
-print ('[3] employees')
-print ('[4] jobs')
-print ('[0] Esci')
-ans=input('Input: ')
+def show_menu():
+    print("Menu:")
+    print("1. Esegui file 1")
+    print("2. Esegui file 2")
+    print("3. Esegui file 3")
+    print("4. Esci")
 
-if ans=="1":
-    os.system('python Query\countries.py')
+def execute_file(file_name):
+    try:
+        exec(open(file_name).read())
+    except FileNotFoundError:
+        print("File non trovato.")
+    except Exception as e:
+        print("Si è verificato un errore durante l'esecuzione del file:", e)
+
+def main():
+    while True:
+        show_menu()
+        choice = input("Seleziona un'opzione: ")
+
+        if choice == "1":
+            execute_file("Query/countries.py")
+        elif choice == "2":
+            execute_file("file2.py")
+        elif choice == "3":
+            execute_file("file3.py")
+        elif choice == "4":
+            break
+        else:
+            print("Scelta non valida. Riprova.")
+
+if __name__ == "__main__":
+    main()
